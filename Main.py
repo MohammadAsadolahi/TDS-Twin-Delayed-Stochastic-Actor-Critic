df2272abef141972e44ef86bac7f15f27b80807f
MohammadAsadolahi <79268727+MohammadAsadolahi@users.noreply.github.com>
2024-07-05 13:07:46 +0330

    Update Main.py
    Import libraries updated

--- Main.py
+++ Main.py
@@ -1,3 +1,21 @@
+import sys
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import animation, rc
+from IPython.display import Math, HTML
+import json,os
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions.normal import Normal
+import copy
+import pandas as pd
+import gymnasium as gym
+
+
 import Requirements,Replay_buffer,Actor,Critic,Agent #provided classes in the TDS github repo
 
 algorithm_name="TDS"
