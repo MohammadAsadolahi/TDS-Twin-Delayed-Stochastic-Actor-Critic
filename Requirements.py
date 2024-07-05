b6f5bd15a5f0979bc899c6cf38a0e8e81b2a5d5f
MohammadAsadolahi <79268727+MohammadAsadolahi@users.noreply.github.com>
2024-07-05 13:06:35 +0330

    Update Requirements.py

--- Requirements.py
+++ Requirements.py
@@ -1,18 +1,12 @@
-import sys
-import numpy as np
-import random
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import animation, rc
-from IPython.display import Math, HTML
-import os
-import torch as T
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.distributions.normal import Normal
-import copy
-import pandas as pd
-import json,os
-!pip install gymnasium[mujoco]
-import gymnasium as gym
+# Use "pip install -r Requirements.txt" to install this file included libraries
+#
+
+numpy
+copy
+random
+gym
+matplotlib.pyplot as plt
+torch
+pdbs
+gymnasium[mujoco]
+pandas
